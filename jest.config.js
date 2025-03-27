61de688617c6dae435bdf54149351a21a2282880
Case Wylie <cmwylie19@defenseunicorns.com>
2025-03-27 15:43:59 +0000

    chore: unit test in a workflow (#9)
    Add a workflow file to run unit tests
    
    ---------
    
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  collectCoverageFrom: ["controls/**/*.ts", "!controls/index.ts"],
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js']
+}
