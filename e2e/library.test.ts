2f40c2ee7537cea917f8924713de322c2655a7dd
Sam Mayer <sam.mayer@defenseunicorns.com>
2025-04-08 11:37:07 -0500

    Add remarks to e2e tests

--- e2e/library.test.ts
+++ e2e/library.test.ts
@@ -10,21 +10,25 @@ describe("compliance-reporter", () => {
     SecurityContext: {
       id: "AC-1",
       description: "Prevents Pod Escalation",
+      remarks: "Some Remarks 1",
     },
 
     Storage: {
       id: "AC-2",
       description: "Prevents Volume Escalation",
+      remarks: "Some Remarks 2",
     },
 
     ServiceMesh: {
       id: "NIST-800-53-5",
       description: "Service Mesh",
+      remarks: "Some Remarks 3",
     },
 
     NetworkPolicy: {
       id: "NIST-800-53-6",
       description: "Network Policy",
+      remarks: "Some Remarks 4",
     },
   })
   const report = generateComplianceReport()
@@ -35,18 +39,22 @@ describe("compliance-reporter", () => {
       SecurityContext: {
         id: "AC-1",
         description: "Prevents Pod Escalation",
+        remarks: "Some Remarks 1",
       },
       Storage: {
         id: "AC-2",
         description: "Prevents Volume Escalation",
+        remarks: "Some Remarks 2",
       },
       ServiceMesh: {
         id: "NIST-800-53-5",
         description: "Service Mesh",
+        remarks: "Some Remarks 3",
       },
       NetworkPolicy: {
         id: "NIST-800-53-6",
         description: "Network Policy",
+        remarks: "Some Remarks 4",
       },
     })
   })
@@ -56,24 +64,28 @@ describe("compliance-reporter", () => {
       "AC-1": {
         id: "AC-1",
         description: "Prevents Pod Escalation",
+        remarks: "Some Remarks 1",
         coveragePercent: 0,
         implementations: [],
       },
       "AC-2": {
         id: "AC-2",
         description: "Prevents Volume Escalation",
+        remarks: "Some Remarks 2",
         coveragePercent: 0,
         implementations: [],
       },
       "NIST-800-53-5": {
         id: "NIST-800-53-5",
         description: "Service Mesh",
+        remarks: "Some Remarks 3",
         coveragePercent: 0,
         implementations: [],
       },
       "NIST-800-53-6": {
         id: "NIST-800-53-6",
         description: "Network Policy",
+        remarks: "Some Remarks 4",
         coveragePercent: 0,
         implementations: [],
       },
@@ -84,24 +96,28 @@ describe("compliance-reporter", () => {
       {
         id: "AC-1",
         description: "Prevents Pod Escalation",
+        remarks: "Some Remarks 1",
         coveragePercent: 0,
         implementations: [],
       },
       {
         id: "AC-2",
         description: "Prevents Volume Escalation",
+        remarks: "Some Remarks 2",
         coveragePercent: 0,
         implementations: [],
       },
       {
         id: "NIST-800-53-5",
         description: "Service Mesh",
+        remarks: "Some Remarks 3",
         coveragePercent: 0,
         implementations: [],
       },
       {
         id: "NIST-800-53-6",
         description: "Network Policy",
+        remarks: "Some Remarks 4",
         coveragePercent: 0,
         implementations: [],
       },
